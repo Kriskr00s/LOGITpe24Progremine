d24d2c12797aa47eed1dddbb0b6fdd026a87109e
Kriskr00s <valtinkristofer@gmail.com>
2025-03-04 09:07:46 +0200

    init

---
+++ 02. 26.02.2025/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data types!");
+
+            //täisnumber
+            int bigNumber = 1234567890;
+            Console.WriteLine(bigNumber);
+
+            //komaga number ??
+            double comaNumber = 12.5;
+            Console.WriteLine(comaNumber);
+
+            //tähemärk
+            string stringVar = "asd456$%^&*(     84u5098lnfigu+-";
+            Console.WriteLine(stringVar);
+
+            float floatVariable = 123.123f;
+            Console.WriteLine(floatVariable);
+
+            char charVariable = '2';
+            Console.WriteLine(charVariable);
+
+            bool boolVariable = false;
+            Console.WriteLine(boolVariable);
+        }
+    }
+}
